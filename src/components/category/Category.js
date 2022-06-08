68e0304c505df79f696a2ff199ae704bbdb22d5f
CastBart <C00205464@itcarlow.ie>
2022-06-08 09:48:29 +0100

    category component test

---
+++ src/components/category/Category.js
@@ -0,0 +1,11 @@
+import { defineArguments } from "graphql/type/definition";
+import { Component } from "react";
+
+class Category extends Component{
+    render(){
+        return <h1>Category</h1>
+    }
+
+}
+
+export default Category;
